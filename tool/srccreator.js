b5cfc8dd1bdbd9cfc3741f0a22e34ff6e23a6f99
383523223@qq.com <383523223@qq.com>
2019-06-17 22:26:25 +0800

    add UserApiStruct

--- tool/srccreator.js
+++ tool/srccreator.js
@@ -38,9 +38,80 @@ function create() {
   fileHelper.copyDir(path.join(__dirname, '../src_tpl'), path.join(__dirname, '../src'), data, '', true);
 }
 
+function readUserApiStruct() {
+  let filepath = path.join(__dirname, '../api/tradeapi_linux64/ThostFtdcUserApiStruct.h');
+
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) throw err;
+    data = data.replace(/\//g, '');
+    let lines = data.split(spliter);
+
+    for(let i = lines.length - 1; i >= 0; i--) {
+      if (/^\s*$/.test(lines[i])) {
+        lines.splice(i, 1);
+      }
+      // else {
+      //   lines[i] = lines[i].replace(/'/g, '"');
+      // }
+    }
+
+    let description;
+
+    for (let i = 0; i < lines.length - 1; i++) {
+      let line = lines[i];
+      let nextLine = lines[i + 1];
+      
+      if (/^TFtdc/.test(line)) {
+        if (/[\u4e00-\u9fa5]+/.test(nextLine)) {
+          description = trim(nextLine);
+          line = line.replace(/^TFtdc/, '').replace('是一个', ' ');
+          let arr = line.split(/\s/);
+          dictText += "\n// " + arr[1] + "\n";
+          enumText += "\n// " + arr[1] + "\n";
+          dictText += "exports." + arr[0] + " = {";
+          enumText += "exports." + arr[0] + " = new Enum(";
+          // i += 1;
+          inDict = true;
+        }
+        else {
+          description = '';
+        }
+      }
+      else if (inDict) {
+        if (description && /^#define\s/.test(line)) {
+          let words = line.split(/\s+/);
+          if (words.length >= 3) {
+            let value = words[2];
+            value = trim(value);
+            let key = words[1];
+            let keys = key.split(/_/);
+            key = keys[keys.length - 1];
+            key = trim(key);
+            dictText += "\n  // " + description + "\n";
+            dictText += "  " + key + ": " + value + ",";
+            enumText += "\n  { alias: '" + key + "', value: " + value + ", text: '" + description + "' },";
+            description = '';
+          }
+        }
+        else if (/[\u4e00-\u9fa5]+/.test(line)) {
+          description = trim(line);
+        }
+        else if(/^typedef/.test(line)) {
+          dictText += "\n};\n";
+          enumText += "\n);\n";
+          inDict = false;
+        }
+      }
+    }
+}
+
 
 function trim(value) {
   return value.replace(/^\s*/, '').replace(/\s*$/, '');
+}
+
+function isChineseCharacters(text) {
+  return /[\u4e00-\u9fa5]+/.test(text);
 }
 
 exports.create = create;
