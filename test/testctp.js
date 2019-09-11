2b4d84b2b43c4d278c387636b4d7c3e4dfdfcadf
weilee <19042049@cnsuning.com>
2019-09-11 20:59:12 +0800

    update testctp

--- test/testctp.js
+++ test/testctp.js
@@ -1,20 +1,20 @@
-const CtpClass = require('../lib/ctp');
+const CTP = require('../lib/ctp');
 
-let ctp = new CtpClass(
+let ctp = new CTP(
     {
         frontend: {
            	BrokerID: '4040',// 银河期货
-           	MdURL: 'tcp://180.166.103.21:51213',
-           	TdURL: 'tcp://180.166.103.21:51205'
+           	MdURL: 'tcp://180.166.103.21:55213',
+           	TdURL: 'tcp://180.166.103.21:55205'
         }
     },
     {
-        InvestorID: '369888',
+        InvestorID: '369868',
         Password: '123456'
     }
 );
 
-// ctp.createTdApi();
+ctp.createTdApi();
 ctp.createMdApi();
 
 // let td = ctp.td;
@@ -30,5 +30,12 @@ ctp.md.On('OnFrontConnected', function() {
     let { md, investor } = ctp;
     console.log('ReqUserLogin : %s', md.ReqUserLogin(investor, ctp.nRequestID()));
     console.log('OnFrontConnected');
+    console.log(ctp.md.SubscribeMarketData(['rb2001'], 1));
+});
+
+ctp.md.On('OnRtnDepthMarketData', function(data) {
+    console.log(data);
 })
+
+
 ctp.registerMdFront();
