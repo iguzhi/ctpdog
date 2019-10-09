ee1534e75e5187167eaa128996c69d2186d17b6d
weilee <19042049@cnsuning.com>
2019-10-09 10:12:35 +0800

    update version = 0.3.2

--- config/logger.js
+++ config/logger.js
@@ -4,10 +4,11 @@ module.exports = {
 		market: 'INFO',
 		trade: 'INFO',
 		ctp: 'INFO',
-		// app: 'INFO',
-		// email: 'INFO',
-		// sql: 'INFO',
-		// websocket: 'INFO',
-		// schedule: 'INFO'
+		// 之所以打开以下category, 是为了兼容之前项目
+		app: 'INFO',
+		email: 'INFO',
+		sql: 'INFO',
+		websocket: 'INFO',
+		schedule: 'INFO'
 	}
 };
